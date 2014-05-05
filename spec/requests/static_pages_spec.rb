ee7a3d6aeea92e91dae38539fc243ed37ce1f9fb
Jason Loo <jmloo@uwaterloo.ca>
2014-05-05 10:27:42 -0400

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,11 @@ describe "StaticPages" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Vion Genesis')
 		end
+
+		it "should have the title'Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Vion Genesis | Home")
+		end
 	end
 
 	describe "Help page" do
@@ -15,6 +20,11 @@ describe "StaticPages" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
 		end
+
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Vion Genesis | Help")
+		end
 	end
 
 	describe "About page" do
@@ -22,6 +32,11 @@ describe "StaticPages" do
 		it "should have the content 'About Us'" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
+		end
+
+		it "should have the title 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Vion Genesis | About Us")
 		end
 	end
 end
