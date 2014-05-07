1ed6d6536d71e5d3e0ff50b10a71501d5267040f
Jason Loo <jmloo@uwaterloo.ca>
2014-05-07 10:02:26 -0400

    add title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,8 +10,15 @@ describe "StaticPages" do
 
 		it "should have the title'Home'" do
 			visit '/static_pages/home'
-			expect(page).to have_title("Vion Genesis | Home")
+			expect(page).to have_title("Vion Genesis")
 		end
+
+		it "should not have the custom page title" do
+			visit '/static_pages/home'
+			expect(page).not_to have_title('| Home')
+		end
+
+
 	end
 
 	describe "Help page" do
