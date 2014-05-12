e30e51222d7fc6c71293fe2ea23c656db2a53e80
Jason Loo <jmloo@uwaterloo.ca>
2014-05-11 23:36:16 -0400

    Javascript NavBar

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,49 +1,61 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-	describe "Home page" do
+describe "Static pages" do
 
-		it "should have the content 'Vion Genesis'" do
-			visit '/static_pages/home'
-			expect(page).to have_content('Vion Genesis')
-		end
+  describe "Home page" do
 
-		it "should have the title'Home'" do
-			visit '/static_pages/home'
-			expect(page).to have_title("Vion Genesis")
-		end
+    it "should have the content 'Vion Genesis'" do
+      visit root_path
+      expect(page).to have_content('Vion Genesis')
+    end
 
-		it "should not have the custom page title" do
-			visit '/static_pages/home'
-			expect(page).not_to have_title('| Home')
-		end
+    it "should have the base title" do
+      visit root_path
+      expect(page).to have_title("Vion Genesis")
+    end
 
+    it "should not have a custom page title" do
+      visit root_path
+      expect(page).not_to have_title('| Home')
+    end
+  end
 
-	end
+  describe "Help page" do
 
-	describe "Help page" do
-		
-		it "should have the content 'Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_content('Help')
-		end
+    it "should have the content 'Help'" do
+      visit help_path
+      expect(page).to have_content('Help')
+    end
 
-		it "should have the title 'Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_title("Vion Genesis | Help")
-		end
-	end
+    it "should have the title 'Help'" do
+      visit help_path
+      expect(page).to have_title("Vion Genesis | Help")
+    end
+  end
 
-	describe "About page" do
-		
-		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
-			expect(page).to have_content('About Us')
-		end
+  describe "About page" do
 
-		it "should have the title 'About Us'" do
-			visit '/static_pages/about'
-			expect(page).to have_title("Vion Genesis | About Us")
-		end
-	end
+    it "should have the content 'About Us'" do
+      visit about_path
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit about_path
+      expect(page).to have_title("Vion Genesis | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("Vion Genesis | Contact")
+    end
+  end
 end
