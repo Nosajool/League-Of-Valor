1c78cd9120142ff85efc1e53b3a03260a15d6fc9
Jason Loo <jmloo@uwaterloo.ca>
2014-05-16 01:20:09 -0400

    User Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,21 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])	
 	end
 	def new
-		
+		@user = User.new		
 	end
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:success] = "Welcome to Vion Genesis"
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
+
+
+	private
+		def user_params
+			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		end
 end
