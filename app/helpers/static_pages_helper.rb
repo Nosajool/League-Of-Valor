2b65eca59a3556baad6846212cf3e13b2bc6618e
Jason Loo <jmloo@uwaterloo.ca>
2014-05-23 21:44:00 -0400

    Created a helper method to create the links to the github issues that have been fixed

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,7 @@
 module StaticPagesHelper
+
+	# Renders a link to the github issue that has been fixed in this patch
+	def git_issue(num)
+		return "<li><a href=\"https://github.com/Nosajool/Vion-Genesis-Rails/issues/#{num}\" id = \"issues\">\##{num}</a></li>".html_safe
+	end
 end
