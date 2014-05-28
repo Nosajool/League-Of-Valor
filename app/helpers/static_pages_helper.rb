a0d9af9620399976a96a22784c6240b79a7adec7
Jason Loo <jmloo@uwaterloo.ca>
2014-05-28 01:02:48 -0400

    Remove stuff not in 1.0.0

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -2,6 +2,6 @@ module StaticPagesHelper
 
 	# Renders a link to the github issue that has been fixed in this patch
 	def git_issue(num)
-		return "<li><a href=\"https://github.com/Nosajool/Vion-Genesis-Rails/issues/#{num}\" id = \"issues\">\##{num}</a></li>".html_safe
+		return "<li><a href=\"https://github.com/Nosajool/League-Of-Valor/issues/#{num}\" id = \"issues\">\##{num}</a></li>".html_safe
 	end
 end
