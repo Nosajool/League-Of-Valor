f8c02f0032acbf97db517760061b6cd3afe8a2e2
Jason Loo <jmloo@uwaterloo.ca>
2014-05-28 01:15:13 -0400

    Add Champion Range to attributes and fix tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526044913) do
+ActiveRecord::Schema.define(version: 20140528050439) do
 
   create_table "table_champions", force: true do |t|
     t.string   "champ_name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140526044913) do
     t.integer  "magic_resist"
     t.string   "role"
     t.integer  "catch_rate"
+    t.integer  "range"
   end
 
   create_table "users", force: true do |t|
