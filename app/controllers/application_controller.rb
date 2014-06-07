f980fec967601c4ef6b67bd059cfc8d5b5732574
Jason Loo <jmloo@uwaterloo.ca>
2014-06-07 15:33:30 -0400

    Add the total user count to the footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :total_users
   include SessionsHelper
+
+  private
+	  def total_users
+	  	@total_users = User.count
+	  end
 end
