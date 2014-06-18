b528bcb7883b77b48266acd7a153d8fc8a860457
Jason Loo <jmloo@uwaterloo.ca>
2014-06-18 00:27:08 -0400

    Take probabilities from data and randomly pick champion

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -6,5 +6,20 @@ class MapsController < ApplicationController
 
   	def show
   		@map = Map.find(params[:id])
+  		# Should get array of champions available on this map
+  		@map_champions = @map.map_champions
+  		num = rand_num
+  		@map_champions.each do |champ|
+  			if rand_num < champ.probability
+  				@random_champ = TableChampion.find(champ.champ_id)
+  				break
+  			end
+  		end
+
   	end
+
+  	private
+  		def rand_num
+  			1 + rand(100)
+  		end
 end
