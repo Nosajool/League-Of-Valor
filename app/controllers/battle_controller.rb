5449b137344c5175bf2085fe0c1cb5cbbdb8a62b
Jason Loo <jmloo@uwaterloo.ca>
2014-06-20 22:50:37 -0400

    add health to champion table and start battle

--- app/controllers/battle_controller.rb
+++ app/controllers/battle_controller.rb
@@ -1,14 +1,50 @@
 class BattleController < ApplicationController
   def battle
-  	flash[:success] = "test"
-  	redirect_to current_user
+    opponent = User.find(params[:opp_id])
+
+    roster = getRoster(current_user)
+    roster_count = roster.count
+    if(roster_count < 1 || roster_count > 5)
+      flash[:error] = "Invalid User Roster"
+      redirect_to champ_select_path(opponent.id)
+    end
+
+
+    opp_roster = getRoster(opponent)
+    opp_roster_count = opp_roster.count
+    if(opp_roster_count <1 || opp_roster_count >5)
+      flash[:error] = "Sorry, your opponent does not have a valid roster"
+      redirect_to champ_select_path(opponent.id)
+    end
+
+    user_health = Array.new
+    roster.each do |champ|
+      user_health
+
+    user_attach_damage = Array.new
+
+
+
   end
 
   def setup
   	if params[:opp_id]
-  		@opponent = User.find(params[:opp_id])
-		@opp_roster = @opponent.champions.where("position != '0'")
-		@roster = current_user.champions.where("position != '0'")
+    	@opponent = User.find(params[:opp_id])
+  		@opp_roster = getRoster(@opponent)
+  		@roster = getRoster(current_user)
   	end
   end
+
+  private
+    def getRoster(user)
+      roster = []
+      # Fill @roster array with empty champions
+      for x in 0..4
+        if user.champions.where("position = #{x+1}").exists?
+          roster << user.champions.where("position = #{x+1}").first
+        end
+      end
+      roster
+    end
+
 end
