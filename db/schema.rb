026d33c06b5a4a337f3eac347142c477305f5f5f
Jason Loo <jmloo@uwaterloo.ca>
2014-07-20 12:19:42 -0400

    user_id index to buffs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140720064828) do
+ActiveRecord::Schema.define(version: 20140720161831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,8 @@ ActiveRecord::Schema.define(version: 20140720064828) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "buffs", ["user_id"], name: "index_buffs_on_user_id", using: :btree
 
   create_table "champions", force: true do |t|
     t.integer  "table_champion_id"
