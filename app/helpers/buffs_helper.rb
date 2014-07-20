6676060742484c576e3233e28bdcb3daa0e35391
Jason Loo <jmloo@uwaterloo.ca>
2014-07-20 13:49:30 -0400

    Buff image integration

--- app/helpers/buffs_helper.rb
+++ app/helpers/buffs_helper.rb
@@ -1,2 +1,12 @@
 module BuffsHelper
+
+	def buff_img(buff)
+		id = buff.id
+		image_tag("#{pre_image_url}buffs/#{id}.png", alt: "Buff Image")		
+	end
+
+	private
+		def pre_image_url
+			"https://raw.githubusercontent.com/Nosajool/League-Of-Valor/images/"
+		end
 end
